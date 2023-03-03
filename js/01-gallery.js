68a4419230eaee8d4ecef2e437189ccb94e889c1
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-03-03 22:17:43 +0200

    01_gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function creatGallery(images) {
+  return images
+    .map(
+      (image) =>
+        `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+        <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+         />
+         </a>
+    </div>`
+    )
+    .join("");
+}
+
+const addGallery = creatGallery(galleryItems);
+galleryContainer.innerHTML = addGallery;
+
+console.log(galleryContainer);
+
+//------- МОДАЛЬНЕ ВІКНО--------
+
+galleryContainer.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width = "800" height ="60">`
+  );
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
